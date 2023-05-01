dc6cf40145c7ea1ca6d0b9470e10b2cbcb2c3538
deepu nair <dpnr915@gmail.com>
2023-05-01 16:44:08 +0530

    building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import axios from 'axios';
 
-interface Game{
+export interface Game{
     id:number;
-    name:String;
+    name:string;
+    background_image:string;
 }
 
 
