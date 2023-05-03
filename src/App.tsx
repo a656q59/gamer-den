44592ed15da2a9aa4798aec3f2e50201a30ad7ba
deepu nair <dpnr915@gmail.com>
2023-05-03 22:15:28 +0530

    sorting the games

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import SortSelector from './components/SortSelector';
 export interface GameQuery{
   genre:Genre|null;
   platform:Platform|null;
+  sortOrder:string;
 }
 
 
@@ -39,7 +40,7 @@ function App() {
       <GridItem  area='main'> 
       <HStack spacing={5} paddingLeft={5} marginBottom={5}>
         <PlaformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform)=>setGameQuery({...gameQuery, platform})}/>  
-        <SortSelector/>
+        <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=>setGameQuery({...gameQuery, sortOrder})}/>
       </HStack>   
       <GameGrid gameQuery={gameQuery}/>  </GridItem>
 
