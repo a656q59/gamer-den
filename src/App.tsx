e31906951e014b76983a219b2e9e6c1ed9815ff0
deepu nair <dpnr915@gmail.com>
2023-05-04 10:25:18 +0530

    adding dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Genre } from './hooks/useGenres';
 import PlaformSelector from './components/PlaformSelector';
 import { Platform } from './hooks/useGames';
 import SortSelector from './components/SortSelector';
+import GameHeading from './components/GameHeading';
 
 
 export interface GameQuery{
@@ -39,13 +40,28 @@ function App() {
       <GridItem  area={'side'} paddingX={5}>    <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre)=>setGameQuery({...gameQuery, genre})}/>  </GridItem>
       </Show>
       <GridItem  area='main'> 
-      <Flex paddingLeft={5} marginBottom={5}>
-        <Box marginRight={5}>
-          <PlaformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform)=>setGameQuery({...gameQuery, platform})}/>  
+
+        <Box paddingLeft={5}>
+
+          <GameHeading gameQuery={gameQuery} />
+
+          <Flex marginBottom={5}>
+
+            <Box marginRight={5}>
+              <PlaformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform)=>setGameQuery({...gameQuery, platform})}/>
+            </Box>
+
+            <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=>setGameQuery({...gameQuery, sortOrder})}/>
+            
+          </Flex>
+
         </Box>
-        <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=>setGameQuery({...gameQuery, sortOrder})}/>
-      </Flex>   
-      <GameGrid gameQuery={gameQuery}/>  </GridItem>
+
+
+        <GameGrid gameQuery={gameQuery}/>  
+
+
+      </GridItem>
 
     </Grid>
   )
