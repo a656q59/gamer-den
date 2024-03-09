15851c730a783f21f79d2d9f05f8ed18c54cd913
a656q59 <dpnr915@gmail.com>
2024-03-09 14:23:25 +0530

    refactoring-removing duplicate interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,83 @@
-import { Box, Flex, Grid, GridItem, HStack } from '@chakra-ui/react'
-import { Show } from '@chakra-ui/react'
-import NavBar from './components/NavBar';
-import GameGrid from './components/GameGrid';
-import GenreList from './components/GenreList';
-import {useState} from 'react';
-import { Genre } from './hooks/useGenres';
-import PlaformSelector from './components/PlaformSelector';
-import { Platform } from './hooks/useGames';
-import SortSelector from './components/SortSelector';
-import GameHeading from './components/GameHeading';
+import { Box, Flex, Grid, GridItem, HStack } from "@chakra-ui/react";
+import { Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
+import PlaformSelector from "./components/PlaformSelector";
+import { Platform } from "./hooks/usePlatforms";
+import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
-
-export interface GameQuery{
-  genre:Genre|null;
-  platform:Platform|null;
-  sortOrder:string;
-  searchText:string
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+  sortOrder: string;
+  searchText: string;
 }
 
-
-
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform|null>(null);
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
-  const[gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
- 
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
   return (
-    <Grid templateAreas={{
-      base:`"nav "" main"`,
-      lg:`"nav nav""side main"`
-    }}
-    templateColumns={{
-      base:'1fr',
-      lg:'200px 1fr'
-    }}>
-      <GridItem   area='nav'>    <NavBar onSearch={(searchText)=>setGameQuery({...gameQuery,searchText})}/>  </GridItem>
-      <Show above='lg'>
-      <GridItem  area={'side'} paddingX={5}>    <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre)=>setGameQuery({...gameQuery, genre})}/>  </GridItem>
+    <Grid
+      templateAreas={{
+        base: `"nav "" main"`,
+        lg: `"nav nav""side main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        {" "}
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />{" "}
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"side"} paddingX={5}>
+          {" "}
+          <GenreList
+            selectedGenre={gameQuery.genre}
+            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+          />{" "}
+        </GridItem>
       </Show>
-      <GridItem  area='main'> 
-
+      <GridItem area="main">
         <Box paddingLeft={5}>
-
           <GameHeading gameQuery={gameQuery} />
 
           <Flex marginBottom={5}>
-
             <Box marginRight={5}>
-              <PlaformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform)=>setGameQuery({...gameQuery, platform})}/>
+              <PlaformSelector
+                selectedPlatform={gameQuery.platform}
+                onSelectPlatform={(platform) =>
+                  setGameQuery({ ...gameQuery, platform })
+                }
+              />
             </Box>
 
-            <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=>setGameQuery({...gameQuery, sortOrder})}/>
-            
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSelectSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+            />
           </Flex>
-
         </Box>
 
-
-        <GameGrid gameQuery={gameQuery}/>  
-
-
+        <GameGrid gameQuery={gameQuery} />
       </GridItem>
-
     </Grid>
-  )
+  );
 }
 
-export default App
+export default App;
