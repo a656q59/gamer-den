b10df5e5e4d3d2be347545fc00c3693ae0f97257
a656q59 <dpnr915@gmail.com>
2024-03-16 00:27:41 +0530

    fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,31 +1,37 @@
-import { Game } from '../hooks/useGames';
-import {Card,CardBody,HStack,Heading, Image, Text} from '@chakra-ui/react';
-import PlatformIconList from './PlatformIconList';
-import CriticScore from './CriticScore';
-import getCroppedImageUrl from '../services/image-url';
-import Emoji from './Emoji';
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
-interface Props{
-    game:Game
+interface Props {
+  game: Game;
 }
 
+const GameCard = ({ game }: Props) => {
+  return (
+    <Link to={"/games/" + game.slug}>
+      {" "}
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
 
-const  GameCard = ({game}:Props) =>{
-    return (
-        <Card >
-            <Image src={getCroppedImageUrl(game.background_image)}/>
-            <CardBody>
-            
-                <HStack justifyContent='space-between' marginBottom={3}>
-                    <PlatformIconList platforms = {game.parent_platforms.map(p=>p.platform)}/>
-                    <CriticScore score={game.metacritic}/>
-                </HStack>
-
-                <Heading fontSize='2xl'>{game.name} <Emoji rating={game.rating_top}/></Heading>
-
-            </CardBody>
-        </Card>
-    );
-}
+          <Heading fontSize="2xl">
+            {game.name}
+            <Emoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Card>
+    </Link>
+  );
+};
 
 export default GameCard;
--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,16 +1,24 @@
-import {ReactNode}from 'react';
-import { Box } from '@chakra-ui/react';
+import { ReactNode } from "react";
+import { Box } from "@chakra-ui/react";
+import { transform } from "framer-motion";
 
-interface Props{
-    children: ReactNode
+interface Props {
+  children: ReactNode;
 }
 
-const GameCardContainer=({children}:Props) =>{
-    return (
-        <Box borderRadius={10} overflow='hidden'>
-            {children}
-        </Box>
-    );
-}
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box
+      borderRadius={10}
+      overflow="hidden"
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
 
 export default GameCardContainer;
