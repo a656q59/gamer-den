df0c72895b613c036bfc54cdaf113d373ec72e5d
a656q59 <dpnr915@gmail.com>
2024-03-16 13:13:55 +0530

    build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,7 @@ import useGame from "../hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
   const { id } = useParams();
@@ -18,6 +19,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
 
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
